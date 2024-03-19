67404e7b008d82026d7975e1f3a0cb96d2477793
Mohammed Benlamlih <benlamlih.med@gmail.com>
2024-03-19 12:39:09 +0100

    inti

---
+++ mappings.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  }
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" }
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
+
+M.lspconfig = {
+  plugin = true,
+  n = {
+    ["gn"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Goto next diagnostic" },
+    ["gp"] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Goto previous diagnostic" },
+  },
+}
+
+return M
